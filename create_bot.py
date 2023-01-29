1bf3f6f991d6418f1504cdb09d93bc89f97818bc
brokendictaphone <123826001+brokendictaphone@users.noreply.github.com>
2023-01-30 01:41:03 +0300

    косметические изменения в файле create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -2,8 +2,8 @@
 from aiogram import Bot
 from configure import config
 from aiogram.dispatcher import Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage #хранилище для данных в оперативной памяти
-storage = MemoryStorage()# экземпляр хранилища
+from aiogram.contrib.fsm_storage.memory import MemoryStorage  # хранилище для данных в оперативной памяти
+storage = MemoryStorage()  # экземпляр хранилища
 
-bot = Bot(token = config)# экземпляр бота
-dp = Dispatcher(bot, storage = storage)# экземпляр диспетчера
+bot = Bot(token=config)  # экземпляр бота
+dp = Dispatcher(bot, storage=storage)  # экземпляр диспетчера
