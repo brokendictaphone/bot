6d9966f5ccc01821c611b5e5ca38a2a9174d8b84
brokendictaphone <123826001+brokendictaphone@users.noreply.github.com>
2023-01-30 17:59:23 +0300

    добавлена кнопка "создать список"

--- keyboard.py
+++ keyboard.py
@@ -1,7 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+b1 = KeyboardButton('создать список')
 b2 = KeyboardButton('показать список')
 b3 = KeyboardButton('/help')
 kb_start = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 # клавиатура создается(кнопки будут изменяться под надписи, клавиатура будет удаляться после использования)
-kb_start.add(b2).row(b3)  # добавление кнопок роу - ряд, адд - простое добавление
+kb_start.add(b1, b2).row(b3)  # добавление кнопок роу - ряд, адд - простое добавление
