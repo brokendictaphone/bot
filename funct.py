0ccaf577b312d08caae04922a66beeffe551e488
brokendictaphone <123826001+brokendictaphone@users.noreply.github.com>
2023-02-22 18:16:49 +0300

    удаление предыдущих сообщений оптимизировано

--- funct.py
+++ funct.py
@@ -6,10 +6,13 @@ async def del_mess(id):
     """удаляет сообщения бота по месседж айди"""
     data_base = sq.connect('ListBotBase2.db')  # связь с БД
     cur = data_base.cursor()
-    for msg_id in cur.execute(f"SELECT msg FROM msg_ids WHERE user_id = {id}"):
+ #   for msg_id in cur.execute(f"SELECT msg FROM msg_ids WHERE user_id = {id}"):
+    cur.execute(f"SELECT msg FROM msg_ids WHERE user_id = {id}")
+    msg_list = cur.fetchall()
+    for msg_id in msg_list:
         await bot.delete_message(id, msg_id[0])
         cur.execute(f'DELETE FROM msg_ids WHERE msg = ? AND user_id = ?', (msg_id[0], id))
-        data_base.commit()  # подтверждение действий
+    data_base.commit()  # подтверждение действий
     data_base.close()  # закрытие ДБ
 
 
@@ -17,10 +20,6 @@ def msg_id_write(msg, id):
     """записывает id сообщения в БД"""
     data_base = sq.connect('ListBotBase2.db')  # добавление данных в список дел
     cur = data_base.cursor()
-    # cur.execute(f"SELECT msg FROM msg_ids WHERE user_id = {id} ")  # выбор значения
-    # if cur.fetchone():
-    #     cur.execute(f'UPDATE msg_ids SET msg = {msg.message_id} WHERE user_id = {id}')
-    # else:
     cur.execute("""INSERT INTO msg_ids(user_id,msg) VALUES(?,?)""",
                     (id, msg.message_id))  # добавление данных
     data_base.commit()  # подтверждение действий
